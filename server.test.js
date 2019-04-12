92e01fab603fed2d8f693ee5f338dd602c1c8345
andrewlowell <andrewlowellr@gmail.com>
2019-04-12 10:13:23 -0600

    Finish all stretch.

--- server.test.js
+++ server.test.js
@@ -7,10 +7,38 @@ describe('test the seeded games server', () => {
     expect(res.status).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
     expect(res.body[0]).toEqual({
+      id: 1,
       title: 'Morrowind', // required
       genre: 'RPG', // required
       releaseYear: 4567 // not required
     });
+  });
+});
+
+describe('test getting by id with a valid id', () => {
+  it('should respond with 200 and an one game when we do GET /games/valid', async () => {
+    const res = await request(server).get('/games/1');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: 1,
+      title: 'Morrowind', // required
+      genre: 'RPG', // required
+      releaseYear: 4567 // not required
+    });
+  });
+});
+
+describe('test getting by id with an invalid id', () => {
+  it('should respond with 404 when we do GET /games/invalid', async () => {
+    const res = await request(server).get('/games/7');
+    expect(res.status).toBe(404);
+  });
+});
+
+describe('test deleting by id with an invalid id', () => {
+  it('should respond with 404 when we do DELETE /games/invalid', async () => {
+    const res = await request(server).delete('/games/7');
+    expect(res.status).toBe(404);
   });
 });
 
@@ -54,7 +82,7 @@ describe('test adding a game without the required fields', () => {
   });
 });
 
-describe('test adding a game without the required fields', () => {
+describe('test adding a game with the required fields', () => {
   it('should respond with 201 and the correct object', async () => {
     const res = await request(server)
       .post('/games')
@@ -68,10 +96,39 @@ describe('test adding a game without the required fields', () => {
     expect(res.status).toBe(201);
     expect(res.body).toEqual(
       {
+        id: 4,
         title: 'Monopoly',
         genre: 'Board',
         releaseYear: 1234
       }
     );
+  });
+});
+
+describe('trying a duplicate title', () => {
+  it('should respond with 405', async () => {
+    const res = await request(server)
+      .post('/games')
+      .send(
+        {
+          title: "Morrowind", // required
+          genre: 'Board', // required
+          releaseYear: null // not required
+        }
+      );
+    expect(res.status).toBe(405);
+  });
+});
+
+describe('test deleting by id with a valid id', () => {
+  it('should respond with 200 and an one game when we do DELETE /games/valid', async () => {
+    const res = await request(server).delete('/games/1');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: 1,
+      title: 'Morrowind', // required
+      genre: 'RPG', // required
+      releaseYear: 4567 // not required
+    });
   });
 });
